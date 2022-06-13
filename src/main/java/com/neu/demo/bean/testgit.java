f43acd0646e19fb9fb5bde2c20a921d1b4ca17ef
curry <curry@lxd.com>
2022-06-13 10:12:53 +0800

    hot-fix commit1

--- src/main/java/com/neu/demo/bean/testgit.java
+++ src/main/java/com/neu/demo/bean/testgit.java
@@ -3,5 +3,6 @@ package com.neu.demo.bean;
 public class testgit {
     public static void main(String[] args) {
         System.out.println("git2");
+        System.out.println("hot-fix-git1");
     }
 }
