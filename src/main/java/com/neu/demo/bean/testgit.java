87b73fc588cbad1e3a72d09105ff0cb670219840
lxdgood <95263467+lxdgood@users.noreply.github.com>
2022-06-13 10:48:58 +0800

    pull test

--- src/main/java/com/neu/demo/bean/testgit.java
+++ src/main/java/com/neu/demo/bean/testgit.java
@@ -6,5 +6,6 @@ public class testgit {
         System.out.println("hot-fix-git1");
         System.out.println("master-git2");
         System.out.println("hot-fix-git2");
+        System.out.println("pull test")
     }
 }
